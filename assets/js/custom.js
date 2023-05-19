0b66f8f751415c5c00e3919d883cbaf62f46e625
Gautam goel <gautamgoel96.2@gmail.com>
2023-05-19 19:47:48 +0530

    updated rahul's branch by replacing custom.js, index.html, custom.css, because we don't know how to work with branches in github, we will learnsoon hahaha

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,5 @@
+// const e = require("express");
+
 let val = setTimeout(popup_func, 5000);
 
 function popup_func(){
@@ -8,3 +10,36 @@ function popup_func(){
 closePopup.addEventListener("click", function () {
         myPopup.classList.remove("show");
 });
+
+function myFunction(x) {
+    console.log("hi");
+    if (x.matches) { // If media query matches
+        const underMediaScroll = document.querySelector('.under-media-scroll');
+        underMediaScroll.style.display = "block";
+
+        const outerMediaMarquee1 = document.querySelector('.outer-media-marquee1');
+        outerMediaMarquee1.style.display = "none";
+        const outerMediaMarquee2 = document.querySelector('.outer-media-marquee2');
+        outerMediaMarquee2.style.display = "none";
+        // element.style.animationPlayState = "paused";
+        // element.style.display = "none";
+        // element.childNode.style.animationName = "none";
+        // console.log(element.parentNode);
+        // element.style.backgroundColor = "black";
+        // element.style.overflow = "auto";
+        // element.style.whiteSpace = "nowrap";
+        
+    } else {
+        const underMediaScroll = document.querySelector('.under-media-scroll');
+        underMediaScroll.style.display = "none";
+
+        const outerMediaMarquee1 = document.querySelector('.outer-media-marquee1');
+        outerMediaMarquee1.style.display = "block";
+        const outerMediaMarquee2 = document.querySelector('.outer-media-marquee2');
+        outerMediaMarquee2.style.display = "block";
+    }
+}
+
+var x = window.matchMedia("(max-width: 1276px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
