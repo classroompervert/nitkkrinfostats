6b46577720468fe8bc6372d4239807ee8e53635b
rahulMaurya140 <rm7655318@gmail.com>
2023-05-24 23:08:17 +0530

    adding comments and removing redundent text from website

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,38 +1,28 @@
-// const e = require("express");
 
+// popup function run after 5 seconds
 let val = setTimeout(popup_func, 5000);
 
 function popup_func(){
-    // console.log("hi");
     myPopup.classList.add("show");
 }
 
+// cross button of popup
 closePopup.addEventListener("click", function () {
         myPopup.classList.remove("show");
 });
 
-function myFunction(x) {
-    console.log("hi");
+
+function marqueeToHorizontalScroll(x) {
     if (x.matches) { // If media query matches
         const underMediaScroll = document.querySelector('.under-media-scroll');
         underMediaScroll.style.display = "block";
-
         const outerMediaMarquee1 = document.querySelector('.outer-media-marquee1');
         outerMediaMarquee1.style.display = "none";
         const outerMediaMarquee2 = document.querySelector('.outer-media-marquee2');
-        outerMediaMarquee2.style.display = "none";
-        // element.style.animationPlayState = "paused";
-        // element.style.display = "none";
-        // element.childNode.style.animationName = "none";
-        // console.log(element.parentNode);
-        // element.style.backgroundColor = "black";
-        // element.style.overflow = "auto";
-        // element.style.whiteSpace = "nowrap";
-        
+        outerMediaMarquee2.style.display = "none";  
     } else {
         const underMediaScroll = document.querySelector('.under-media-scroll');
         underMediaScroll.style.display = "none";
-
         const outerMediaMarquee1 = document.querySelector('.outer-media-marquee1');
         outerMediaMarquee1.style.display = "block";
         const outerMediaMarquee2 = document.querySelector('.outer-media-marquee2');
@@ -41,5 +31,5 @@ function myFunction(x) {
 }
 
 var x = window.matchMedia("(max-width: 1276px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
+marqueeToHorizontalScroll(x) // Call listener function at run time
+x.addListener(marqueeToHorizontalScroll) // Attach listener function on state changes
